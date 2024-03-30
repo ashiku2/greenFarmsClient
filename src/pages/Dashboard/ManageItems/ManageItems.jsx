5ded0dcbbbdcfa97ad22b9954806beb538a12b3a
ashiku2 <ashikur1602@gmail.com>
2024-03-30 07:01:57 +0600

    manage item done

--- src/pages/Dashboard/ManageItems/ManageItems.jsx
+++ src/pages/Dashboard/ManageItems/ManageItems.jsx
@@ -1,10 +1,97 @@
 import React from 'react';
 import SectionTitle from '../../../components/SectionTitle/SectionTitle';
+import useMenu from '../../../hooks/useMenu';
+import { FaTrash, FaUpload } from 'react-icons/fa6';
+import { FaEdit } from 'react-icons/fa';
+import Swal from 'sweetalert2';
+import useAxiosSecure from '../../../hooks/useAxiosSecure';
+import useAxiosPublic from '../../../hooks/useAxiosPublic';
 
 const ManageItems = () => {
+    const [menu, , refetch] = useMenu();
+    const axiosSecure = useAxiosSecure();
+    const axiosPublic = useAxiosPublic();
+    const handleDeleteItem = (item) => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then(async (result) => {
+            if (result.isConfirmed) {
+                const res = await axiosSecure.delete(`/menu/${item._id}`);
+
+                if (res.data.deletedCount > 0) {
+                    refetch();
+                    Swal.fire({
+                        title: "Deleted!",
+                        text: "Your file has been deleted.",
+                        icon: "success"
+                    });
+                }
+                
+            }
+        });
+    }
+
     return (
         <div>
             <SectionTitle heading={"Manage All Items"} subHeading={"Hurry up"}></SectionTitle>
+            <div>
+                <div className="overflow-x-auto">
+                    <table className="table">
+                        {/* head */}
+                        <thead>
+                            <tr>
+                                <th>
+                                   #
+                                </th>
+                                <th>Image</th>
+                                <th>Item Name</th>
+                                <th>Price</th>
+                                <th>Update</th>
+                                <th>Delete</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {/* row 1 */}
+                            {
+                                menu.map((item, index) => <tr key={item._id}>
+                                    <th>
+                                        {index+1}
+                                    </th>
+                                    <td>
+                                        <div className="flex items-center gap-3">
+                                            <div className="avatar">
+                                                <div className="mask mask-squircle w-12 h-12">
+                                                    <img src={item.image} />
+                                                </div>
+                                            </div>
+
+                                        </div>
+                                    </td>
+                                    <td>
+                                        {item.name}
+                                    </td>
+                                    <td>${item.price}</td>
+                                    <th>
+                                        <button onClick={() => handleDelete(item._id)} className="btn btn-ghost btn-2xl"><FaEdit></FaEdit></button>
+                                    </th>
+                                    <td>
+                                        <button onClick={() => handleDeleteItem(item)} className="btn btn-ghost btn-2xl"><FaTrash></FaTrash></button>
+                                    </td>
+                                </tr>)
+                            }
+                            
+                        </tbody>
+                        
+
+                    </table>
+                </div>
+            </div>
         </div>
     );
 };
